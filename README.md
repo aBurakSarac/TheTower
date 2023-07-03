0126c2a008de7d33adb90c4ce704396b3e5ceb5a
aBurakSarac <alibsarac@gmail.com>
2023-07-03 15:56:27 +0300

    Added explanations

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # git_practice-again
+
+The explanation below is written by ChatGPT
+
+
+This code is a skeleton structure for a game called "The Tower" using the Pygame library. Here's a breakdown of what the code does:
+
+It imports necessary modules and initializes the Pygame modules.
+It sets up the window dimensions and creates a Pygame window.
+It defines some colors using RGB values.
+It sets the frames per second (FPS) and the velocity of the block.
+It sets up the font to display text.
+It defines a function draw_window() to draw the game window with the floor and a start message if the game has not started yet.
+It defines an empty function handle_block() to update the state and position of the block.
+It defines an empty function draw_score() to draw the score on the screen.
+It sets up the game clock.
+It enters the main game loop (while run), which continues until the run variable is set to False.
+Inside the loop, it checks for events (like quitting the game or pressing a key).
+If the event is a key press and the pressed key is the space key, and the game has not started yet, it sets is_started to True.
+It then calls handle_block() to update the block position and state, and draw_window() to draw the game window with the updated information.
+Please note that the code provided is a skeleton and contains some empty functions (handle_block() and draw_score()) that need to be implemented with the desired game logic.
--- theTower.py
+++ theTower.py
@@ -1,64 +1,81 @@
 import pygame as pg
 import os
+#Version 0.1.1
+
+# Initialize pygame modules
 pg.font.init()
 pg.mixer.init()
 
+# Set up the window dimensions
 WIDTH, HEIGHT = 500, 1000
 WIN = pg.display.set_mode((WIDTH, HEIGHT))
 pg.display.set_caption("The Tower")
 
-#COLORS
+# Define colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 GREEN = (0, 128, 0)
 SKY = (48, 172, 255)
 
-FPS = 60 
-VEL = 8 #SPEED OF BLOCK
+# Set the frames per second and velocity of the block
+FPS = 60
+VEL = 8
 
+# Set up the font for displaying text
 PLAY_FONT = pg.font.SysFont("arial", 32)
 
-def draw_window(floor, is_started): #VISUALIZATION OF ITEMS
-    WIN.fill(SKY)
-    pg.draw.rect(WIN,GREEN, floor)
+def draw_window(floor, is_started):
+    """
+    Draw the game window with the floor and a start message if the game has not started yet.
+    
+    Args:
+        floor (pygame.Rect): Rectangle representing the floor.
+        is_started (bool): Flag indicating if the game has started or not.
+    """
+    WIN.fill(SKY)  # Fill the window with sky color
+    pg.draw.rect(WIN, GREEN, floor)  # Draw the floor
     draw_text = PLAY_FONT.render("PRESS SPACE TO START", 1, WHITE)
     if is_started == False:
-        WIN.blit(draw_text, (WIDTH/2 - draw_text.get_width() /
-                         2, HEIGHT/2 - draw_text.get_height()/2))
+        # If the game has not started yet, display the start message at the center of the window
+        WIN.blit(draw_text, (WIDTH/2 - draw_text.get_width() / 2, HEIGHT/2 - draw_text.get_height()/2))
     pg.display.update()
-        
 
-def handle_block(): #TODO
+def handle_block():
+    """
+    Update the state and position of the block.
+    """
+    # TODO: Add the implementation for handling the block
     pass
 
-def draw_score(): #TODO
+def draw_score():
+    """
+    Draw the score on the screen.
+    """
+    # TODO: Add the implementation for drawing the score
     pass
-
 def main():
+    # Set up the game clock
     clock = pg.time.Clock()
+
     run = True
-    floor = pg.Rect(0, 900, WIDTH, 100)
+    floor = pg.Rect(0, 900, WIDTH, 100)  # Create a rectangle representing the floor
     is_started = False
-    #block = pg.Rect(0,800,256,100)
-    
 
     while run:
-        clock.tick(FPS)
+        clock.tick(FPS)  # Limit the game loop to a specific frame rate
+
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 run = False
                 pg.quit()
-            if event.type == pg.KEYDOWN: 
+
+            if event.type == pg.KEYDOWN:
                 if event.key == pg.K_SPACE and is_started == False:
                     is_started = True
 
-        handle_block()    
-        draw_window(floor, is_started)
+        handle_block()  # Update the block position and state
+        draw_window(floor, is_started)  # Draw the game window
         
 
 if __name__ == "__main__":
     main()
-
-
-
-
